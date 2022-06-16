92af5143002cbe1e436ccdab3a4355a7d5212f3e
AlexisComix <thecomixguy@hotmail.com>
2022-06-16 13:05:51 +0100

    Add function to update a list with coordinates of the tail segments to be drawn

--- snakeclasses.py
+++ snakeclasses.py
@@ -1,5 +1,6 @@
 # Classes for the snake
 import pygame
+from collections import deque
 
 class NewSnake:
     def __init__(self, window: pygame.display) -> None:
@@ -10,12 +11,14 @@ class NewSnake:
         self.win_height = self.win.get_height()
         self.win_width = self.win.get_width()
 
-        self.length = 5                     # Starting length
+        self.length = 0                     # Not counting head
         self.x, self.y = 0, 0               # Starting head coords
         self.SIZE = 20                      # Size of squares
 
         self.velocity = 1                   
         self.vector = (self.velocity, 0)    # Default vector, velocity to the right
+
+        self.tail_coords = [(self.x, self.y)]      # List to store tail coords
 
 
     def head(self) -> pygame.Rect:
@@ -59,3 +62,14 @@ class NewSnake:
         """
         self.x += self.vector[0] * self.SIZE
         self.y += self.vector[1] * self.SIZE
+
+
+    def tail(self) -> None:
+        """
+        Function that updates the list with the coordinates of the tail to draw
+        """
+        dequeue = deque(self.tail_coords)           # dequeue object
+        shifted = dequeue.rotate(1)                 # shift coords to the right
+        shift_list = list(shifted)                  # convert object into list
+        shift_list[0] = (self.x, self.y)            # add head coords to tail
+        self.tail_coords = shift_list               # update self.tail_coords
